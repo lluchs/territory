6c46d05b87e19b497ea1cc5b4bb2072a679f4138
Lukas Werling <lukas@lwrl.de>
2020-04-15 19:02:02 +0200

    Update fish configuration

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,5 +6,19 @@ if set -q SSH_AUTH_SOCK
   set -e SSH_AUTH_SOCK
 end
 
-# Add homeshick function.
-source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+fish_vi_key_bindings
+bind -M insert jk 'set fish_bind_mode default; commandline -f backward-char force-repaint'
+
+set fish_color_command green
+set fish_color_operator brblue --bold
+set fish_color_param brblue
+set fish_color_comment brblack
+set fish_color_error brred --bold
+set fish_color_quote yellow
+set fish_color_escape cyan
+set fish_color_redirection magenta
+set fish_color_end brred
+
+set fish_color_host normal
+set fish_color_user brblue --bold
+set fish_color_cwd white --bold
